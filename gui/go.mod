03dfa5d3869f1d32ba7b5d08214d43ace1483f8a
Biang <595974382@qq.com>
2023-05-01 08:38:14 +0800

    修改使用fyne框架制作客户端

--- gui/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/BiangUncle/splendor/gui
-
-go 1.18
-
-require github.com/maxence-charriere/go-app/v9 v9.6.7
-
-require github.com/google/uuid v1.3.0 // indirect
--- gui/main.go
+++ gui/main.go
@@ -1,46 +1,21 @@
 package main
 
 import (
-	"github.com/BiangUncle/splendor/gui/game"
-	"github.com/maxence-charriere/go-app/v9/pkg/app"
-	"log"
-	"net/http"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	// The first thing to do is to associate the hello component with a path.
-	//
-	// This is done by calling the Route() function,  which tells go-app what
-	// component to display for a given path, on both client and server-side.
-	app.Route("/table", &game.Game{})
-	// Once the routes set up, the next thing to do is to either launch the app
-	// or the server that serves the app.
-	//
-	// When executed on the client-side, the RunWhenOnBrowser() function
-	// launches the app,  starting a loop that listens for app events and
-	// executes client instructions. Since it is a blocking call, the code below
-	// it will never be executed.
-	//
-	// When executed on the server-side, RunWhenOnBrowser() does nothing, which
-	// lets room for server implementation without the need for precompiling
-	// instructions.
-	app.RunWhenOnBrowser()
+	a := app.New()
+	w := a.NewWindow("window")
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
 
-	// Finally, launching the server that serves the app is done by using the Go
-	// standard HTTP package.
-	//
-	// The Handler is an HTTP handler that serves the client and all its
-	// required resources to make it work into a web browser. Here it is
-	// configured to handle requests with a path that starts with "/".
-	http.Handle("/", &app.Handler{
-		Name:        "Hello",
-		Description: "An Hello World! example",
-		Styles: []string{
-			"/web/splendor.css",
-		},
-	})
-
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Fatal(err)
-	}
+	w.ShowAndRun()
 }
